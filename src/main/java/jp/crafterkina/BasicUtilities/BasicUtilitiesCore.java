c5e1b572dee2bf32345345f3610ad768793bdc9c
CrafterKina <kina_ugo@yahoo.co.jp>
2016-02-04 18:57:01 +0900

    add logging

--- src/main/java/jp/crafterkina/BasicUtilities/BasicUtilitiesCore.java
+++ src/main/java/jp/crafterkina/BasicUtilities/BasicUtilitiesCore.java
@@ -11,13 +11,19 @@ import jp.crafterkina.BasicUtilities.processor.annotation.register.GameRegistrar
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(modid = "jp.crafterkina.BasicUtilities")
 public class BasicUtilitiesCore{
+    public static final Logger logger = LogManager.getLogger("jp.crafterkina.BasicUtilities");
     @EventHandler
     public void preInit(FMLPreInitializationEvent event){
+        logger.debug("Start Interpret DataTable");
         ASMDataTableInterpreter.instance.init(event);
+        logger.debug("Start Config Parse");
         ConfigProcessor.parseConfigs();
+        logger.debug("Start Game Register");
         GameRegistrar.start();
     }
 }
