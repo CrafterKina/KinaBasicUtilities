cfc6961b5f000410c2ba7eb502b6979c1cf4e630
CrafterKina <kina_ugo@yahoo.co.jp>
2016-02-04 16:45:41 +0900

    create NBT Parsing System

---
+++ src/main/java/jp/crafterkina/BasicUtilities/nbt/NBTParser.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2016, CrafterKina
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package jp.crafterkina.BasicUtilities.nbt;
+
+import com.google.common.base.Splitter;
+import com.google.common.reflect.Reflection;
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.common.util.Constants;
+
+import java.lang.annotation.*;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.ListIterator;
+
+public class NBTParser{
+
+    public static <T> T parse(final NBTTagCompound compound, Class<? extends T> clazz){
+        return Reflection.newProxy(clazz, new InvocationHandler(){
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable{
+                if(compound == null) return null;
+                NBTBase result = returnNBT(compound, method.getAnnotation(Return.class));
+                insertNBT(compound, method.getParameterAnnotations(), args);
+                return result;
+            }
+        });
+    }
+
+    private static void insertNBT(NBTTagCompound compound, Annotation[][] annotations, Object[] args){
+        for(int i = 0; i < annotations.length; i++){
+            Annotation[] annotation = annotations[i];
+            Object arg = args[i];
+            for(Annotation ann : annotation){
+                if(!(ann instanceof Insert)) continue;
+                Insert insert = (Insert) ann;
+                List<String> list = Splitter.on(insert.separator()).splitToList(insert.value());
+                int j = 0;
+                for(; j < (list.size() - 1); j++){
+                    String s = list.get(j);
+                    if(!compound.hasKey(s, Constants.NBT.TAG_COMPOUND)){
+                        compound.setTag(s, new NBTTagCompound());
+                    }
+                    compound = compound.getCompoundTag(s);
+                }
+                compound.setTag(list.get(j), (NBTBase) arg);
+            }
+        }
+    }
+
+    private static NBTBase returnNBT(NBTTagCompound compound, Return ann){
+        if(ann == null) return null;
+        NBTBase cache = compound;
+        if(ann.value().isEmpty() && !compound.hasKey(ann.value())){
+            return compound;
+        }
+        for(ListIterator<String> iterator = Splitter.on(ann.separator()).splitToList(ann.value()).listIterator(); iterator.hasNext(); ){
+            String next = iterator.next();
+            if(cache instanceof NBTTagCompound){
+                if(((NBTTagCompound) cache).getTag(next) == null){
+                    ((NBTTagCompound) cache).setTag(next, cache = iterator.hasNext() ? new NBTTagCompound() : null);
+                }else{
+                    cache = ((NBTTagCompound) cache).getTag(next);
+                }
+            }else{
+                throw new IllegalArgumentException("Wrong tag");
+            }
+        }
+        return cache;
+    }
+
+
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface Return{
+        String value();
+
+        String separator() default "/";
+    }
+
+    @Target(ElementType.PARAMETER)
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface Insert{
+        String value();
+
+        String separator() default "/";
+    }
+}
---
+++ src/test/java/jp/crafterkina/BasicUtilities/nbt/NBTParserTest.java
@@ -0,0 +1,264 @@
+/*
+ * Copyright (c) 2016, CrafterKina
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package jp.crafterkina.BasicUtilities.nbt;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagInt;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+public class NBTParserTest{
+
+    /**
+     * Try to insert compound of itemstack to "parent/itemstack" with or without NBTParser. Assert that the with is the
+     * without.
+     */
+    @Test
+    public void testInsertChildNBTTagCompound() throws Exception{
+        NBTTagCompound target = new NBTTagCompound();
+        NBTTagCompound parser = new NBTTagCompound();
+        NBTTagCompound correct = new NBTTagCompound();
+        {
+            Gist parsed = NBTParser.parse(parser, Gist.class);
+            parsed.setChildItemStack(target);
+        }
+        {
+            NBTTagCompound parent = new NBTTagCompound();
+            correct.setTag("parent", parent);
+            parent.setTag("itemstack", target);
+        }
+        assertThat(parser, is(correct));
+    }
+
+    /**
+     * Try to insert integer to "parent/itemstack" with or without NBTParser. Assert that the with is the without.
+     */
+    @Test
+    public void testInsertChildNBTTagInt() throws Exception{
+        NBTTagInt target = new NBTTagInt(30);
+        NBTTagCompound parser = new NBTTagCompound();
+        NBTTagCompound correct = new NBTTagCompound();
+        {
+            Gist parsed = NBTParser.parse(parser, Gist.class);
+            parsed.setChildInteger(target);
+        }
+        {
+            NBTTagCompound parent = new NBTTagCompound();
+            correct.setTag("parent", parent);
+            parent.setTag("integer", target);
+        }
+        assertThat(parser, is(correct));
+    }
+
+    /**
+     * Try to insert compound of itemstack to "itemstack" with or without NBTParser. Assert that the with is the
+     * without.
+     */
+    @Test
+    public void testInsertParentNBTTagCompound() throws Exception{
+        NBTTagCompound target = new NBTTagCompound();
+        NBTTagCompound parser = new NBTTagCompound();
+        NBTTagCompound correct = new NBTTagCompound();
+        {
+            Gist parsed = NBTParser.parse(parser, Gist.class);
+            parsed.setParentItemStack(target);
+        }
+        {
+            correct.setTag("itemstack", target);
+        }
+        assertThat(parser, is(correct));
+    }
+
+    /**
+     * Try to insert integer to "integer" with or without NBTParser. Assert that the with is the without.
+     */
+    @Test
+    public void testInsertParentNBTTagInt() throws Exception{
+        NBTTagInt target = new NBTTagInt(30);
+        NBTTagCompound parser = new NBTTagCompound();
+        NBTTagCompound correct = new NBTTagCompound();
+        {
+            Gist parsed = NBTParser.parse(parser, Gist.class);
+            parsed.setParentInteger(target);
+        }
+        {
+            correct.setTag("integer", target);
+        }
+        assertThat(parser, is(correct));
+    }
+
+    /**
+     * Try to get compound of itemstack from "parent/itemstack" with or without NBTParser. Assert that the with is the
+     * without.
+     */
+    @Test
+    public void testReturnChildNBTTagCompound() throws Exception{
+        NBTTagCompound target = new NBTTagCompound();
+        NBTTagCompound parser;
+        NBTTagCompound correct;
+        {
+            target.setTag("parent", new NBTTagCompound());
+            target.getCompoundTag("parent").setTag("itemstack", new NBTTagCompound());
+        }
+        {
+            Gist parsed = NBTParser.parse(target, Gist.class);
+            parser = parsed.getChildItemStack();
+        }
+        {
+            NBTTagCompound parent = target.getCompoundTag("parent");
+            correct = parent.getCompoundTag("itemstack");
+        }
+        assertThat(parser, is(correct));
+    }
+
+    /**
+     * Try to get integer from "parent/integer" with or without NBTParser. Assert that the with is the without.
+     */
+    @Test
+    public void testReturnChildNBTTagInt() throws Exception{
+        NBTTagCompound target = new NBTTagCompound();
+        int parser;
+        int correct;
+        {
+            target.setTag("parent", new NBTTagCompound());
+            target.getCompoundTag("parent").setTag("integer", new NBTTagInt(30));
+        }
+        {
+            Gist parsed = NBTParser.parse(target, Gist.class);
+            parser = parsed.getChildInteger().getInt();
+        }
+        {
+            NBTTagCompound parent = target.getCompoundTag("parent");
+            correct = parent.getInteger("integer");
+        }
+        assertThat(parser, is(correct));
+    }
+
+    /**
+     * Try to get compound of itemstack from "itemstack" with or without NBTParser. Assert that the with is the
+     * without.
+     */
+    @Test
+    public void testReturnParentNBTTagCompound() throws Exception{
+        NBTTagCompound target = new NBTTagCompound();
+        NBTTagCompound parser;
+        NBTTagCompound correct;
+        {
+            target.setTag("itemstack", new NBTTagCompound());
+        }
+        {
+            Gist parsed = NBTParser.parse(target, Gist.class);
+            parser = parsed.getParentItemStack();
+        }
+        {
+            correct = target.getCompoundTag("itemstack");
+        }
+        assertThat(parser, is(correct));
+    }
+
+    /**
+     * Try to get integer from "integer" with or without NBTParser. Assert that the with is the without.
+     */
+    @Test
+    public void testReturnParentNBTTagInt() throws Exception{
+        NBTTagCompound target = new NBTTagCompound();
+        int parser;
+        int correct;
+        {
+            target.setTag("integer", new NBTTagInt(30));
+        }
+        {
+            Gist parsed = NBTParser.parse(target, Gist.class);
+            parser = parsed.getParentInteger().getInt();
+        }
+        {
+            correct = target.getInteger("integer");
+        }
+        assertThat(parser, is(correct));
+    }
+
+    /**
+     * Try to get not exist tag with NBTParser. Assert that the with is null value.
+     */
+    @Test
+    public void testReturnEmpty() throws Exception{
+        NBTTagCompound target = new NBTTagCompound();
+        NBTTagInt parser;
+        {
+            Gist parsed = NBTParser.parse(target, Gist.class);
+            parser = parsed.getChildInteger();
+        }
+        assertThat(parser, is(nullValue()));
+    }
+
+    /**
+     * Try to get base with NBTParser. Assert that the with is null value.
+     */
+    @Test
+    public void testReturnBase() throws Exception{
+        NBTTagCompound compound = new NBTTagCompound();
+        Gist parsed = NBTParser.parse(compound, Gist.class);
+        assertThat(parsed.getBase(), is(theInstance(compound)));
+    }
+
+    /**
+     * Try to get tag with NBTParser, but wrong type. Assert that throws ClassCastException.
+     */
+    @Test(expected = ClassCastException.class)
+    public void testWrongReturnType() throws Exception{
+        NBTTagCompound target = new NBTTagCompound();
+        {
+            target.setTag("integer", new NBTTagCompound());
+        }
+        {
+            Gist parsed = NBTParser.parse(target, Gist.class);
+            parsed.getParentInteger();
+        }
+    }
+
+    /**
+     * Try to get tag with NBTParser, but wrong path. Assert that throws IllegalArgumentException.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testWrongReturnPath() throws Exception{
+        NBTTagCompound target = new NBTTagCompound();
+        {
+            target.setTag("parent", new NBTTagInt(-1));
+        }
+        {
+            Gist parsed = NBTParser.parse(target, Gist.class);
+            parsed.getChildInteger();
+        }
+    }
+
+
+    interface Gist{
+        @NBTParser.Return("parent/itemstack")
+        NBTTagCompound getChildItemStack();
+
+        void setChildItemStack(@NBTParser.Insert("parent/itemstack") NBTTagCompound compound);
+
+        @NBTParser.Return("parent/integer")
+        NBTTagInt getChildInteger();
+
+        void setChildInteger(@NBTParser.Insert("parent/integer") NBTTagInt tagInt);
+
+        @NBTParser.Return("itemstack")
+        NBTTagCompound getParentItemStack();
+
+        void setParentItemStack(@NBTParser.Insert("itemstack") NBTTagCompound compound);
+
+        @NBTParser.Return("integer")
+        NBTTagInt getParentInteger();
+
+        void setParentInteger(@NBTParser.Insert("integer") NBTTagInt tagInt);
+
+        @NBTParser.Return("")
+        NBTTagCompound getBase();
+    }
+}
